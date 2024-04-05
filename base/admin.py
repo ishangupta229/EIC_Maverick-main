d50860eff4e7998fbd2b308261b55b2ac12a90e0
Ishan Gupta <ishangupta12345@yahoo.com>
2024-04-06 02:14:38 +0530

    up

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Profile
+
+admin.site.register(Profile)
+
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- base/models.py
+++ base/models.py
@@ -7,3 +7,13 @@ from django.contrib.auth.models import User
 class MyModel(models.Model):
     Item_name = models.CharField(max_length=200)
     Quant_ity = models.IntegerField()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add additional fields for your profile
+    bio = models.TextField(blank=True)
+    # Add more fields as needed
+
+    def str(self):
+        return self.user.username
